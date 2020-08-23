494a577c21a447fac5e5c263dd2b89d20b730558
LeandroJavierLaino <leanlaino@gmail.com>
2020-08-23 00:49:06 -0300

    Updates

---
+++ precache-manifest.bd4a8ca2837fb48cbeb71ef2041c11fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a04a2a66057bbe689cfc668780c89f",
+    "url": "/portfolioandbio/index.html"
+  },
+  {
+    "revision": "8c34929453c141649ba5",
+    "url": "/portfolioandbio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7800adf476f48142d5a5",
+    "url": "/portfolioandbio/static/js/2.e1f6a347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolioandbio/static/js/2.e1f6a347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c34929453c141649ba5",
+    "url": "/portfolioandbio/static/js/main.bbd7b8ad.chunk.js"
+  },
+  {
+    "revision": "d556bcbed7e740a302bf",
+    "url": "/portfolioandbio/static/js/runtime-main.8e150d75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolioandbio/static/media/logo.5d5d9eef.svg"
+  }
+]);

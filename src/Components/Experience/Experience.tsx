fd8d01c572f6ff329ee4a94903c174c6b8855181
LeandroJavierLaino <leanlaino@gmail.com>
2020-11-09 01:56:13 -0300

    - Completing experiences stepper. - Adding experience translations.

--- src/Components/Experience/Experience.tsx
+++ src/Components/Experience/Experience.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import {
     Paper,
     Typography,
@@ -7,51 +8,200 @@ import {
     createStyles,
     Stepper,
     Step,
-    StepButton
+    StepButton,
+    Button,
+    MobileStepper,
+    StepLabel,
+    StepIconProps,
+    Card,
+    CardContent,
+    CardHeader,
+    Avatar
 } from '@material-ui/core';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import { useTranslation } from 'react-i18next';
 
 function getSteps() {
-    return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+    return [
+        'Ecosistemas - Pernod Ricard Argentina',
+        'Ecosistemas - Monsanto Argentina',
+        'Ecosistemas - Monsanto Argentina',
+        'UTN - FRBA',
+        'Ecosistemas - Bayer Argentina',
+        'Khem Labs'
+    ];
 }
+
+const getStepContent = (step: number) => {
+    switch (step) {
+        case 0:
+            return 'pernodSupportContent';
+        case 1:
+            return 'monsantoSupportContent';
+        case 2:
+            return 'monsantoSysadminContent';
+        case 3:
+            return 'utnHelperContent';
+        case 4:
+            return 'bayerSysadminContent';
+        case 5:
+            return 'khemLabsDeveloperContent';
+        default:
+            return 'unkownStep';
+    }
+};
+
+const getStepDates = (step: number) => {
+    switch (step) {
+        case 0:
+            return 'pernodSupportDate';
+        case 1:
+            return 'monsantoSupportDate';
+        case 2:
+            return 'monsantoSysadminDate';
+        case 3:
+            return 'utnHelperDate';
+        case 4:
+            return 'bayerSysadminDate';
+        case 5:
+            return 'khemLabsDeveloperDate';
+        default:
+            return 'unkownStep';
+    }
+};
+
+const getStepPosition = (step: number) => {
+    switch (step) {
+        case 0:
+            return 'pernodSupportPosition';
+        case 1:
+            return 'monsantoSupportPosition';
+        case 2:
+            return 'monsantoSysadminPosition';
+        case 3:
+            return 'utnHelperPosition';
+        case 4:
+            return 'bayerSysadminPosition';
+        case 5:
+            return 'khemLabsDeveloperPosition';
+        default:
+            return 'unkownStep';
+    }
+};
+
+const QontoStepIcon = (props: StepIconProps) => {
+    const classes = useStyles();
+    const { active } = props;
+
+    return (
+        <div
+            className={clsx(classes.root, {
+                [classes.active]: active
+            })}
+        >
+            <div className={classes.circle} />
+        </div>
+    );
+};
 
 const Experience = () => {
     const { t } = useTranslation();
     const classes = useStyles();
     const steps = getSteps();
     const [activeStep, setActiveStep] = React.useState<number>(0);
-    const [completed, setCompleted] = React.useState(new Set<number>());
 
     const handleStep = (step: number) => () => {
         setActiveStep(step);
     };
 
-    const isStepComplete = (step: number) => {
-        return completed.has(step);
+    const handleNext = () => {
+        const newActiveStep = activeStep + 1;
+        setActiveStep(newActiveStep);
     };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const newContentPosition = getStepContent(activeStep);
+    const newDatePosition = getStepDates(activeStep);
+    const newPosition = getStepPosition(activeStep);
 
     return (
         <Paper>
             <Typography className={classes.title} variant="h5">
-                {t('Experience') + 'aaaa'}
-                <Stepper alternativeLabel nonLinear activeStep={0}>
-                    {steps.map((label, index) => {
-                        const stepProps = {};
-                        const buttonProps = {};
+                {t('Experience')}
 
+                <Stepper alternativeLabel nonLinear activeStep={activeStep}>
+                    {steps.map((label, index) => {
                         return (
-                            <Step key={label} {...stepProps}>
-                                <StepButton
-                                    onClick={handleStep(index)}
-                                    completed={isStepComplete(index)}
-                                    {...buttonProps}
-                                >
-                                    {label}
+                            <Step key={label}>
+                                <StepButton onClick={handleStep(index)}>
+                                    <StepLabel
+                                        StepIconComponent={QontoStepIcon}
+                                    >
+                                        {label}
+                                    </StepLabel>
                                 </StepButton>
                             </Step>
                         );
                     })}
                 </Stepper>
+                <Card>
+                    <CardHeader
+                        avatar={<Avatar src=""></Avatar>}
+                        title={steps[activeStep]}
+                    />
+                    <CardContent>
+                        <Typography
+                            variant="subtitle1"
+                            className={classes.instructions}
+                        >
+                            {t(newPosition)}
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            className={classes.instructions}
+                        >
+                            {t(newDatePosition)}
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            className={classes.instructions}
+                        >
+                            {t(newContentPosition)}
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <MobileStepper
+                    steps={steps.length}
+                    variant="dots"
+                    position="static"
+                    activeStep={activeStep}
+                    nextButton={
+                        <Button
+                            disabled={activeStep === steps.length - 1}
+                            size="small"
+                            onClick={handleNext}
+                            className={classes.button}
+                        >
+                            {t('next')}
+                            <KeyboardArrowRight />
+                        </Button>
+                    }
+                    backButton={
+                        <Button
+                            disabled={activeStep === 0}
+                            onClick={handleBack}
+                            size="small"
+                            className={classes.button}
+                        >
+                            <KeyboardArrowLeft />
+                            {t('back')}
+                        </Button>
+                    }
+                />
             </Typography>
         </Paper>
     );
@@ -72,6 +222,28 @@ const useStyles = makeStyles((theme: Theme) =>
             display: 'flex',
             flexDirection: 'row',
             flexWrap: 'wrap'
+        },
+        button: {
+            marginRight: theme.spacing(1)
+        },
+        instructions: {
+            marginTop: theme.spacing(1),
+            marginBottom: theme.spacing(1)
+        },
+        circle: {
+            width: 8,
+            height: 8,
+            borderRadius: '50%',
+            backgroundColor: 'currentColor'
+        },
+        root: {
+            color: '#eaeaf0',
+            display: 'flex',
+            height: 22,
+            alignItems: 'center'
+        },
+        active: {
+            color: '#784af4'
         }
     })
 );

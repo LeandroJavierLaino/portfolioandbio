c15913eae727c89e58107c3b9c2a9d615335a4fa
LeandroJavierLaino <leanlaino@gmail.com>
2021-01-30 13:16:05 -0300

    - Adding dark theme switching. - Correcting reder some component depending screen size. - Modifying some text. - Changing text buttons for icon buttons. - Adding experience icons.

--- src/Components/Main/MainComponent.tsx
+++ src/Components/Main/MainComponent.tsx
@@ -1,27 +1,48 @@
-import React from "react";
-import { CssBaseline, Toolbar } from "@material-ui/core";
-import CustomAppBar from "../CustomAppBar/CustomAppBar";
-import Summary from "../Summary/Summary";
-import { Skills } from "../Skills/Skills";
-import { Portfolio } from "../Portfolio/Portfolio";
-import { Contact } from "../Contact/Contact";
-import { SocialMedia } from "../SocialMedia/SocialMedia";
-import Experience from "../Experience/Experience";
+import React, { useState } from 'react';
+import {
+    createMuiTheme,
+    CssBaseline,
+    ThemeProvider,
+    Toolbar
+} from '@material-ui/core';
+import CustomAppBar from '../CustomAppBar/CustomAppBar';
+import Summary from '../Summary/Summary';
+import { Skills } from '../Skills/Skills';
+import { Portfolio } from '../Portfolio/Portfolio';
+import { Contact } from '../Contact/Contact';
+import { SocialMedia } from '../SocialMedia/SocialMedia';
+import Experience from '../Experience/Experience';
 
 const MainComponent = () => {
-	return (
-		<React.Fragment>
-			<CssBaseline />
-			<CustomAppBar />
-			<Toolbar />
-			<Summary />
-			<Skills />
-			<Experience />
-			<Portfolio />
-			<Contact />
-			<SocialMedia />
-		</React.Fragment>
-	);
+    const [selectedTheme, setSelectedTheme] = useState<'dark' | 'light'>(
+        'light'
+    );
+
+    const toggleTheme = () => {
+        setSelectedTheme(selectedTheme === 'dark' ? 'light' : 'dark');
+    };
+
+    const themeSelected = createMuiTheme({
+        palette: {
+            type: selectedTheme
+        }
+    });
+
+    return (
+        <React.Fragment>
+            <ThemeProvider theme={themeSelected}>
+                <CssBaseline />
+                <CustomAppBar theme={selectedTheme} toggleTheme={toggleTheme} />
+                <Toolbar />
+                <Summary />
+                <Skills />
+                <Experience />
+                <Portfolio />
+                <Contact />
+                <SocialMedia />
+            </ThemeProvider>
+        </React.Fragment>
+    );
 };
 
 export default MainComponent;
